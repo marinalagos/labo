af81439c4450841ab5d1d283813ba801aa94b709
marinalagos <mlmarinalagos@gmail.com>
2022-10-30 18:27:25 -0300

    ampliación del numero de envios

--- micompetencia3/_z992_ZZ_lightgbm_under.r
+++ micompetencia3/_z992_ZZ_lightgbm_under.r
@@ -153,7 +153,7 @@ for( i in  1:PARAM$modelos )
 
   #genero los archivos para Kaggle
   cortes  <- seq( from=  7000,
-                  to=   11000,
+                  to=   20000,
                   by=     500 )
 
 
