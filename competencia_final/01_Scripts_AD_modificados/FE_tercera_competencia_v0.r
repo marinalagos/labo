b66183f86b0af58bd7de3347b01f161f4abcdd6e
marinalagos <mlmarinalagos@gmail.com>
2022-11-19 03:44:25 -0300

    error de tipeo y skip first column en ds_probas

--- competencia_final/01_Scripts_AD_modificados/FE_tercera_competencia_v0.r
+++ competencia_final/01_Scripts_AD_modificados/FE_tercera_competencia_v0.r
@@ -29,7 +29,7 @@ semillas <- c(732497,
 dataset <- fread("./datasets/competenciaFINAL_2022.csv.gz")
 
 # Cargamos el dataset de probabilidades
-ds_probas <- fread("./datasets/df_probas.csv")
+ds_probas <- fread("./datasets/df_probas.csv", select=c("numero_de_cliente", "foto_mes", "prob_media"))
 
 #### Analisis descriptivo de fechas
 
@@ -446,7 +446,7 @@ AgregarVariables  <- function( dataset )
 
 #------------------------------------------------------------------------------
 #Agregar columna de probabilidad
-dataset <- merge(dataset, df_probas, by = c('numero_de_cliente', 'foto_mes'))
+dataset <- merge(dataset, ds_probas, by = c('numero_de_cliente', 'foto_mes'))
 
 #------------------------------------------------------------------------------
 #deflaciona por IPC
