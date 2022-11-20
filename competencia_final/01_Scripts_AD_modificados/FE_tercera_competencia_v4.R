79dcb8b27dadf1f8b73898a50597bb7e9fe7bbb5
marinalagos <mlmarinalagos@gmail.com>
2022-11-20 12:11:57 -0300

    merge con probas antes de grabar DR

--- competencia_final/01_Scripts_AD_modificados/FE_tercera_competencia_v4.R
+++ competencia_final/01_Scripts_AD_modificados/FE_tercera_competencia_v4.R
@@ -539,6 +539,8 @@ switch(
   "deflacion"      = drift_deflacion( campos_monetarios ) 
 )
 
+ds_probas <- fread("./datasets/df_probas.csv", select=c("numero_de_cliente", "foto_mes", "prob_media"))
+dataset <- merge(dataset, ds_probas, by = c('numero_de_cliente', 'foto_mes'), all=TRUE)
 
 
 fwrite( dataset,
