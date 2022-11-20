bfa365eff295d18a253a7bc193042b99800caff5
marinalagos <mlmarinalagos@gmail.com>
2022-11-20 10:53:27 -0300

    cambio de inner a outer join

--- competencia_final/01_Scripts_AD_modificados/agregar_columna_proba.R
+++ competencia_final/01_Scripts_AD_modificados/agregar_columna_proba.R
@@ -6,7 +6,7 @@ dataset <- fread("./datasets/competenciaFINAL_2022.csv.gz")
 
 # Cargamos el dataset de probabilidades
 ds_probas <- fread("./datasets/df_probas.csv", select=c("numero_de_cliente", "foto_mes", "prob_media"))
-dataset <- merge(dataset, ds_probas, by = c('numero_de_cliente', 'foto_mes'))
+dataset <- merge(dataset, ds_probas, by = c('numero_de_cliente', 'foto_mes'), all=TRUE)
 
 fwrite( dataset,
         "./datasets/competenciaFINAL_2022_PROBA.csv.gz",
